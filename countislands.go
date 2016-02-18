9014be40331ebb5cc6e12fbc1bd558b449c47bd3
Kousik Sundar <kousiksundar@gmail.com>
2016-02-17 22:25:56 -0800

    Island problem passes testcases

--- countislands.go
+++ countislands.go
@@ -1,6 +1,41 @@
 package main
 
-func CountIslands(grid [][]int) int {
-	// TODO: add your code here!
-	return 0
+
+
+func CountIslands(input [][]int) int {
+	if len(input)==0 || len(input[0])==0{
+     return 0;
+    }
+    count:= 0;
+ 
+    for i:=0; i<len(input); i++ {
+        for j:=0; j<len(input[0]); j++ {
+            if input[i][j] == 1 {
+                count++;
+                merge(input, i, j);
+            }
+        }
+    }
+    return count;
+}
+
+
+func merge(input [][]int, i int, j int){
+    
+    if i<0 || j<0 || i>len(input)-1 || j>len(input[0])-1 {
+        return ;
+    }
+    //if current cell is water or visited
+    if input[i][j] != 1 {
+        return;
+ }
+    //set visited cell to '0'
+    input[i][j] =0;
+
+     
+    //merge all adjacent land
+    merge(input, i-1, j);
+    merge(input, i+1, j);
+    merge(input, i, j-1);
+    merge(input, i, j+1);
 }
